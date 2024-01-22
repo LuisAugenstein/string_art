fixed string matrix computation edge cases

string_art/entities/pin.py
@@ -56,15 +56,15 @@ class Pin:
         a_points, b_points = self.corner_points, pinB.corner_points
 
         for i, b_point in enumerate(b_points):
-            diffs = (a_points - b_point[None, :])
-            distances = np.linalg.norm(diffs, axis=1)
-            angles = np.arctan2(diffs[:, 1], diffs[:, 0])
+            distances = np.linalg.norm((a_points - b_point[None, :]), axis=1)
+            angles = self.__get_normalized_angles(b_point)
+            thresh = 1e-8
 
-            i_max_angles = np.where(angles == np.max(angles))[0]
+            i_max_angles = np.where(np.abs(angles - np.max(angles)) < thresh)[0]
             i_max_angle = i_max_angles[np.argmin(distances[i_max_angles])]
             lineAB = np.vstack([a_points[i_max_angle], b_point])
 
-            i_min_angles = np.where(angles == np.min(angles))[0]
+            i_min_angles = np.where(np.abs(angles - np.min(angles)) < thresh)[0]
             i_min_angle = i_min_angles[np.argmin(distances[i_min_angles])]
             lineBA = np.vstack([b_point, a_points[i_min_angle]])
 
@@ -89,19 +89,17 @@ class Pin:
                         best_lengths[2+j] = length
         return lines
 
-    def intersects_string(self, line: Line, threshold=1e-8) -> bool:
+    def __get_normalized_angles(self, base_point: np.ndarray) -> np.ndarray:
         """
-        Parameters
-        -
-        p1: np.shape[2] array of the first point of the string segment
-        p2: np.shape[2] array of the second point of the string segment
+        Computes the relative angles from the given base_point to the corners of the pin.
 
         Returns
         -
-        True if the string segment intersects the pin, False otherwise
+        angles: [0, a1, a2, a3]   angles in radians. first angle is always 0. The remaining 3 angles are relative to the first angle.
         """
-        distances = distance(line, self.corner_points)
-        return np.any(distances <= threshold)
-
-    def angle_to_x_axis(self, v: np.ndarray) -> float:
-        return -np.arctan2(v[1], v[0])
+        diffs = base_point[:, None] - self.corner_points.T  # (2,4)
+        first_angle = np.arctan2(diffs[1, 0], diffs[0, 0])
+        rot_mat_inverse = np.array([[np.cos(first_angle), -np.sin(first_angle)],
+                                    [np.sin(first_angle), np.cos(first_angle)]]).T
+        diff_rot = rot_mat_inverse @ diffs
+        return np.arctan2(diff_rot[1, :], diff_rot[0, :])

string_art/run.py
@@ -7,7 +7,7 @@ from string_art.preprocessing import create_circular_mask, preprocess_image
 from string_art.optimization import LoggingCallback, PlottingCallback
 
 
-def run(config: Config):
+def run(img: np.ndarray, config: Config):
     img = preprocess_image(img, config.low_res, config.invert_input)
     mask = create_circular_mask(config.low_res)
 

tests/test_matlab_string_matrices.py
@@ -1,9 +1,8 @@
-from string_art.io import load_string_matrices
-from string_art.config import get_default_config
+import numpy as np
 from scipy.io import loadmat
 from scipy.sparse import find, csr_matrix
-import numpy as np
-from string_art.preprocessing import high_res_to_low_res_indices, high_res_to_low_res_indices_optimized
+from string_art.config import get_default_config
+from string_art.preprocessing import precompute_string_matrix, high_res_to_low_res_indices, high_res_to_low_res_indices_optimized
 from tests.utils import measure_time
 
 
@@ -13,10 +12,20 @@ def test_with_matlab_string_matrices():
     """
     config = get_default_config()
     config.n_pins = 16
-    A_high_res, _, _ = load_string_matrices(config.n_pins, config.pin_side_length,
-                                            config.string_thickness, config.min_angle, config.high_res, config.low_res)
+    A_high_res, _ = precompute_string_matrix(config.n_pins, config.pin_side_length, config.string_thickness, config.min_angle, config.high_res)
     A_high_res_matlab = loadmat(f'tests/data/A_high_res.mat')['A']
-    assert sparse_matrices_all_close(A_high_res, A_high_res_matlab, first_n_edges=50)
+
+    def sparse_matrices_all_close(A: csr_matrix, B: csr_matrix, first_n_edges=None) -> bool:
+        if first_n_edges is None:
+            first_n_edges = A.shape[1]
+        for edge_index in range(first_n_edges):
+            i, _, v = find(A[:, edge_index])
+            i2, _, v2 = find(B[:, edge_index])
+            if not np.allclose(i, i2) or not np.allclose(v, v2):
+                return False
+        return True
+
+    assert sparse_matrices_all_close(A_high_res, A_high_res_matlab)
 
 
 def test_string_matrix_resizing():
@@ -31,14 +40,3 @@ def test_string_matrix_resizing():
     sort_indices = np.argsort(i_new)
     assert np.allclose(i_new[sort_indices], i_matlab) and np.allclose(v_new[sort_indices], v_matlab)
     assert np.allclose(i_new2[sort_indices], i_matlab) and np.allclose(v_new2[sort_indices], v_matlab)
-
-
-def sparse_matrices_all_close(A: csr_matrix, B: csr_matrix, first_n_edges=None) -> bool:
-    if first_n_edges is None:
-        first_n_edges = A.shape[1]
-    for edge_index in range(first_n_edges):
-        i, j, v = find(A[:, edge_index])
-        i2, j2, v2 = find(B[:, edge_index])
-        if not np.allclose(i, i2) or not np.allclose(j, j2) or not np.allclose(v, v2):
-            return False
-    return True